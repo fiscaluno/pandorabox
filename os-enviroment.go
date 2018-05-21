cf72a3d3d573847c65e1b6df3ce04584092f9b54
julioc98 <julioc.santos98@gmail.com>
2018-05-21 15:53:00 -0300

    remove: Lower Case on func GetOSEnvironment

--- os-enviroment.go
+++ os-enviroment.go
@@ -2,13 +2,12 @@ package pandorabox
 
 import (
 	"os"
-	"strings"
 )
 
 // GetOSEnvironment get a env var or set to default
 func GetOSEnvironment(env string, defaultEnv string) string {
 	if e := os.Getenv(env); e != "" {
-		return strings.ToLower(os.Getenv(env))
+		return os.Getenv(env)
 	}
 	return defaultEnv
 }
